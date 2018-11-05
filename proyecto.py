f25861a6b102d5eea6b322bc9e501ab49ec5b492
gusta257 <33338454+gusta257@users.noreply.github.com>
2018-11-05 11:58:43 -0600

    Ya mete a matriz y multiplica falta mod 29

--- proyecto.py
+++ proyecto.py
@@ -5,17 +5,51 @@ from conversion_letras import *
 archivo = open("frase.txt","r")
 contenido = archivo.read()                  #Lectura del archivo
 contenido = contenido.upper()               #Convierte su contenido a mayusculas
-print contenido                     
-mensaje = letra_numero(contenido)           #Funcion para pasar a numeros (arreglar numeros del 00-09)
-print mensaje
+print contenido
+#mensaje = letra_numero(contenido)           #Funcion para pasar a numeros (arreglar numeros del 00-09)
+#print mensaje
+
 largo =len(contenido)
 largo= int(m.ceil(largo/3))
+print largo
+encript = np.zeros((3,largo))              #Creacion matriz de la llave
+
+diccionario = " ABCDEFGHIJKLMNOPQRSTUVWXYZ!?"
+
+total = len(contenido)
+i = 0
+numero = ""
+letra = ""
+temp = 0
+f=0
+g=0
+cont = 1 
+for i in range(0,total):
+    letra = contenido[i]
+    temp = diccionario.find(letra)
+    
+    numero = numero + str(temp)
+    
+    
+   
+    encript[f,g] = temp
+    f +=1
+    if (cont % 3==0):
+        
+        f = 0
+        g +=1
+    cont +=1
+
+    
+
+
+print encript
 
 llave = np.array([[0, 0, 0],
                   [0, 0, 0],
                   [0, 0, 0]])               #Creacion matriz de la llave
 
-encript = np.zeros((3,largo))              #Creacion matriz de la llave
+
 i=0
 j=0
 contador = 1
@@ -37,16 +71,6 @@ print "El determinante es "+ str(det)
 if(det ==0):
     print "La matriz que ingreso no es invertible por lo que no se puede encriptar"
 
-f=0
-g=0
-cont = 1 
-for letra in mensaje:
-    encript[f,g] = letra
-    f +=1
-    if (cont % 3==0):
-        f = 0
-        g +=1
-    cont +=1
     
 
 print encript
