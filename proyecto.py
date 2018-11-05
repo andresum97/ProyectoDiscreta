ce4a3c1c71053b5586bd2cf60f7f06b2efeb2581
gusta257 <33338454+gusta257@users.noreply.github.com>
2018-11-05 13:03:32 -0600

    Ya funciona cifrado

--- proyecto.py
+++ proyecto.py
@@ -1,18 +1,14 @@
 import numpy as np
-import math as m
+import math 
 from conversion_letras import *
 
 archivo = open("frase.txt","r")
 contenido = archivo.read()                  #Lectura del archivo
 contenido = contenido.upper()               #Convierte su contenido a mayusculas
 print contenido
-#mensaje = letra_numero(contenido)           #Funcion para pasar a numeros (arreglar numeros del 00-09)
-#print mensaje
-
 largo =len(contenido)
-largo= int(m.ceil(largo/3))
-print largo
-encript = np.zeros((3,largo))              #Creacion matriz de la llave
+largo1= int(math.ceil(largo/3.0))
+encript = np.zeros((3,largo1))              #Creacion matriz de la llave
 
 diccionario = " ABCDEFGHIJKLMNOPQRSTUVWXYZ!?"
 
@@ -23,33 +19,23 @@ letra = ""
 temp = 0
 f=0
 g=0
-cont = 1 
+cont =  1
 for i in range(0,total):
     letra = contenido[i]
     temp = diccionario.find(letra)
-    
     numero = numero + str(temp)
-    
-    
-   
     encript[f,g] = temp
     f +=1
     if (cont % 3==0):
-        
         f = 0
         g +=1
     cont +=1
-
-    
-
 
 print encript
 
 llave = np.array([[0, 0, 0],
                   [0, 0, 0],
                   [0, 0, 0]])               #Creacion matriz de la llave
-
-
 i=0
 j=0
 contador = 1
@@ -71,11 +57,13 @@ print "El determinante es "+ str(det)
 if(det ==0):
     print "La matriz que ingreso no es invertible por lo que no se puede encriptar"
 
-    
 
-print encript
-    
 respuesta = llave.dot(encript)
+
+
+
+respuesta = modulo_29(respuesta, largo1)
+respuesta = numero_letra(respuesta,largo1)
 print "la respuesta es"
 print respuesta
 
