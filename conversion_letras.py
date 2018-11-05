c372000f481f8278596d8e8c4d49b420cfda4a70
andresum97 <andresum1309@gmail.com>
2018-11-05 13:22:26 -0600

    Matriz inversa modulo 29 ya la saca, para la llave

--- conversion_letras.py
+++ conversion_letras.py
@@ -1,9 +1,27 @@
-
+import numpy as np
+import math as m
+#Constante donde estan los valores del abecedario
 diccionario = " ABCDEFGHIJKLMNOPQRSTUVWXYZ!?"
-
+"""
 def letra_numero(palabra):
     palabra = palabra.upper()
     total = len(palabra)
+    i = 0
+    numero = ""
+    letra = ""
+    temp = 0
+    for i in range(0,total):
+        letra = palabra[i]
+        temp = diccionario.find(letra)
+        numero = numero + str(temp)
+    return numero
+        """
+        
+#Transforma la palabra entera en valores de numeros
+def letra_numero(palabra):
+    palabra = palabra.upper()   #Todo lo pasa a mayuscular
+    total = len(palabra)        #Largo de la palabra
+    resultado=[ ]               #Lista donde se guardan los numeros
     i = 0
     numero = ""
     letra = ""
@@ -17,8 +35,18 @@ def letra_numero(palabra):
             numl = "0"+str(temp)
         else:
             numl = str(temp)
-        numero = numero + numl
-    return numero
+        numero = numl
+        resultado.append(numero)
+    return resultado
+    #largo = len(palabra)
+    #largo = int(m.ceil(largo/3))
+    #np.zeros((3,largo))
+    #c = np.asarray(resultado)
+    #print c
+    #for i in range(0,len(resultado)):
+     #   print resultado[i]
+        #numero = numero + numl
+    #print numero
 
 
 def modulo_29(matriz,largo):
@@ -31,14 +59,57 @@ def modulo_29(matriz,largo):
             matriz[i][j] = valor29
     return matriz
 
-def numero_letra(matriz,l):
+def numero_letra(matriz,largo):
     valor = 0
     palabra = ""
     letra = ""
-    largo = int(l)
     for j in range(0,largo):
         for i in range(0,3):
             valor = matriz[i][j]
-            letra = diccionario[int(valor)]
+            letra = diccionario[valor]
             palabra += letra
     return palabra
+
+###======================== DESENCRIPCION ===============================
+def generalizedEuclidianAlgorithm(a, b):
+    if b > a:
+        return generalizedEuclidianAlgorithm(b,a);
+    elif b == 0:
+        return (1, 0);
+    else:
+        (x, y) = generalizedEuclidianAlgorithm(b, a % b);
+        return (y, x - (a / b) * y)
+
+def inversemodp(a, p):
+    a = a % p
+    if (a == 0):
+        print "a is 0 mod p"
+        return None
+    if a > 1 and p % a == 0:
+        return None
+    (x,y) = generalizedEuclidianAlgorithm(p, a % p);
+    inv = y % p
+    assert (inv * a) % p == 1
+    return inv
+
+def identitymatrix(n):
+    return [[long(x == y) for x in range(0, n)] for y in range(0, n)]
+
+def inversematrix(matrix):
+    q = 29
+    n = len(matrix)
+    A = np.matrix([[ matrix[j, i] for i in range(0,n)] for j in range(0, n)])
+    Ainv = np.matrix(identitymatrix(n))
+    for i in range(0, n):
+        factor = inversemodp(A[i,i], q)
+        if factor is None:
+             raise ValueError("TODO: deal with this case")
+        A[i] = A[i] * factor % q
+        Ainv[i] = Ainv[i] * factor % q
+        for j in range(0, n):
+            if (i != j):
+                factor = A[j, i]
+                A[j] = (A[j] - factor * A[i]) % q
+                Ainv[j] = (Ainv[j] - factor * Ainv[i]) % q
+    return Ainv
+            
