ca3df0b2aeebad27829b0c9a6d42de7b26578a18
gusta257 <33338454+gusta257@users.noreply.github.com>
2018-11-05 13:01:20 -0600

    Ya funciona el encriptado

--- conversion_letras.py
+++ conversion_letras.py
@@ -1,27 +1,9 @@
-import numpy as np
-import math as m
+
 diccionario = " ABCDEFGHIJKLMNOPQRSTUVWXYZ!?"
-"""
-def letra_numero(palabra):
-    palabra = palabra.upper()
-    total = len(palabra)
-    i = 0
-    numero = ""
-    letra = ""
-    temp = 0
-    for i in range(0,total):
-        letra = palabra[i]
-        temp = diccionario.find(letra)
-        numero = numero + str(temp)
-    return numero
-        """
-        
-#diccionario = " ABCDEFGHIJKLMNOPQRSTUVWXYZ!?"
 
 def letra_numero(palabra):
     palabra = palabra.upper()
     total = len(palabra)
-    resultado=[ ]
     i = 0
     numero = ""
     letra = ""
@@ -35,18 +17,8 @@ def letra_numero(palabra):
             numl = "0"+str(temp)
         else:
             numl = str(temp)
-        numero = numl
-        resultado.append(numero)
-    return resultado
-    #largo = len(palabra)
-    #largo = int(m.ceil(largo/3))
-    #np.zeros((3,largo))
-    #c = np.asarray(resultado)
-    #print c
-    #for i in range(0,len(resultado)):
-     #   print resultado[i]
-        #numero = numero + numl
-    #print numero
+        numero = numero + numl
+    return numero
 
 
 def modulo_29(matriz,largo):
@@ -59,14 +31,14 @@ def modulo_29(matriz,largo):
             matriz[i][j] = valor29
     return matriz
 
-def numero_letra(matriz,largo):
+def numero_letra(matriz,l):
     valor = 0
     palabra = ""
     letra = ""
+    largo = int(l)
     for j in range(0,largo):
         for i in range(0,3):
             valor = matriz[i][j]
-            letra = diccionario[valor]
+            letra = diccionario[int(valor)]
             palabra += letra
     return palabra
-            
